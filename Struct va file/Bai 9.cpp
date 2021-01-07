da1e3addc4aec25b8dfb15e1172231cf00469267
Van-Ni <73803737+Van-Ni@users.noreply.github.com>
2021-01-07 18:55:06 +0700

    Add files via upload

---
+++ Struct va file/Bai 9.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void nhapMang(char  s[], int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("Nhap phan tu thu s[%d]: ", i);
+		scanf("%c", &s[i]);
+		fflush(stdin);
+	}
+}
+void ghiTep(char s[], int n)
+{
+	FILE *f;
+	f=fopen("nguyenam.txt", "w");
+	for(int i=0;i<n;i++)
+	{
+		if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' )
+		fprintf(f," %c ", s[i]);
+	}
+	fclose(f);
+}
+void docTep()
+{
+	FILE *f;
+	f=fopen("nguyenam.txt", "r");
+	int dem=0;
+	char chu, kq;
+	while(fscanf(f," %c ", &chu)>0)
+	{
+		if(chu=='a')
+		dem++;
+	}
+	printf("Co %d a nguyen am", dem);
+	fclose(f);
+}
+main()
+{
+	char a[10];
+	nhapMang(a,5);
+	ghiTep(a,5);
+	docTep();
+}
